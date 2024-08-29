ed15aac91785195761fff3f1c00affe1394c7972
Soumya Ranjan Mishra <118365110+smranjanmishra@users.noreply.github.com>
2024-08-29 08:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let angle = 0;
+
+function rotateLine() {
+    const line = document.getElementById('line');
+    angle += 2;
+    line.style.transform = `translateY(-50%) rotate(${angle}deg)`;
+    requestAnimationFrame(rotateLine);
+}
+
+rotateLine();
